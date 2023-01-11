f4d4e41ab45aa9cffaf14da07c9e43bd8001688f
Ahmet <Ahmet@IAMAHMETKOCA>
2023-01-11 23:27:49 +0300

    Add project files.

---
+++ captcha-projesi/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace captcha_projesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(89, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 47);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(277, 176);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Transitions with captcha";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(31, 98);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(217, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(217, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "İnject";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(122, 299);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            this.label2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(301, 198);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Captcha Example";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ captcha-projesi/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace captcha_projesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text;
+            text= textBox1.Text;
+            label2.Text = text.ToString();
+
+            if (label1.Text == label2.Text)
+            {
+                Form2 frm = new Form2();
+                frm.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("You entered an incorrect value. Try again", "Error");
+                Random rnd = new Random();
+                s1 = rnd.Next(0, harfler.Length);
+                s2 = rnd.Next(0, semboller.Length);
+                s3 = rnd.Next(0, bharfler.Length);
+                s4 = rnd.Next(0, 10);
+                s5 = rnd.Next(0, 10);
+
+                label1.Text = s4.ToString() + harfler[s1].ToString() + semboller[s2].ToString() + s5.ToString() + bharfler[s3].ToString();
+
+                textBox1.Text = "" ;
+            }
+        }
+        string[] harfler = { "a", "b", "c", "d", "e", "f", "g", "h", "ı" };
+        string[] semboller = { "!", "^#", "+", "%", "&", "/", "=", "*", "-" };
+        string[] bharfler = { "A", "B", "C", "D", "E", "F", "G", "H", "I" };
+
+        int s1, s2, s3, s4, s5;
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            Random rnd = new Random();
+            s1 = rnd.Next(0, harfler.Length);
+            s2 = rnd.Next(0, semboller.Length);
+            s3 = rnd.Next(0, bharfler.Length);
+            s4 = rnd.Next(0, 10);
+            s5 = rnd.Next(0, 10);
+
+            label1.Text = s4.ToString() + harfler[s1].ToString() + semboller[s2].ToString() + s5.ToString() + bharfler[s3].ToString();
+        }
+    }
+}
